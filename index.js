d094787f8e4a7e928a448bd969a42186d6a131c6
muhammadali010 <muhammadjonovmuhammadali32@gmail.com>
2024-05-19 14:38:46 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,160 @@
+// 1chi masala
+// let a = + prompt("son kiriting");
+// if (a > 0){
+//         ++a;
+// }
+// document.write(a);
+
+
+// 2chi masala
+// let a = + prompt("son kiriting");
+// if (a > 0){
+//         ++a;
+// } else {
+//            a -=2;
+// }
+// document.write(a);
+
+
+
+// 3chi masala
+// let a = + prompt("son kiriting");
+// if (a > 0){
+//         ++a;
+// } else {
+//            a -=2;
+// }
+// document.write(a);
+
+
+// 6chi masala
+// let a =+ prompt("bitta son kiriting");
+// let b =+ prompt("yana son kiriting");
+// if (a>b){
+//    document.write("a katta");
+// } else{
+// document.write("b katta");
+// }
+
+
+// 7chi masala
+// let a =+ prompt("bitta son kiriting");
+// let b =+ prompt("yana son kiriting");
+// if (a<b){
+//    document.write("a kichkina");
+// } else{
+// document.write("b kichkina");
+// }
+
+
+// 8chi masala
+// let a = + prompt("bitta son kiriting");
+// let b = + prompt("ikkinchi sonni kiriting");
+// if (a > b) {
+//     document.write(a);
+//     document.write("va");
+//     document.write(b);
+// }
+// if (a < b) {
+//     document.write(b);
+//     document.write("va")
+//     document.write(a);
+// }
+
+
+// 11chi masala
+// let a = + prompt("birinchi sonni kiriting");
+// let b =+ prompt ("ikkinchi sonni kiriting");
+// if (a == b){
+// document.write(a+b);
+// }
+// if (a >b ){
+// document.write(a);
+// }
+// if (a < b){
+// document.write(b);
+// }
+// if (a==b){
+// document.write(0);
+// }
+
+
+// 12chi masala
+//  let a = + prompt("birinchi raqam");
+//  let b =+ prompt("ikkinchi raqam");
+//  let c =+ prompt("uchinchi raqam");
+//  if (a<c & a<b){
+//     document.write(a);
+//  }
+//  if  (b<a & b < c){
+//     document.write(b);
+//  }
+//  if (c<a & c<b){
+// document.write(c);
+// }
+
+
+// 13chi masala
+// let a = + prompt("bitta son kiriting");
+// let b = + prompt("ikkinchi soni kiriting");
+// let c = + prompt("uchinchi soni kiriting");
+// if (b < a & a < c) {
+//     document.write(a);
+// }
+// if (b > a & b < c) {
+//     document.write(b);
+// }
+// if (c > a & c < b) {
+//     document.write(c);
+// }
+// if (c< a & b < c) {
+//     document.write(c);
+// }
+// if (a > b & b > c) {
+//     document.write(b);
+// }
+// if (a < b & a > c) {
+//     document.write(a);
+// }
+
+
+// 14chi masala
+// let a = + prompt("bitta son kiriting");
+// let b = + prompt("ikkinchi soni kiriting");
+// let c = + prompt("uchinchi soni kiriting");
+// if (b > a & a < c) {
+//     document.write(a);
+// }
+// if (b < a & b < c) {
+//     document.write(b);
+// }
+// if (c < a & c < b) {
+//     document.write(c);
+// }
+// if (b< a & b < c) {
+//     document.write(a);
+// }
+// if (a < b & b > c) {
+//     document.write(b);
+// }
+// if (c >a & a > b) {
+//     document.write(c);
+// }
+
+
+// 15chi masala
+// let a = + prompt("bitta son kiriting");
+// let b = + prompt("ikkinchi soni kiriting");
+// let c = + prompt("uchinchi soni kiriting");
+// document.write("   yegindisi  ")
+// document.write(a+b+c);
+// document.write("   kattasi  ");
+// if (b < a & a > c) {
+//     document.write(a);
+// }
+// if (b > a & b > c) {
+//     document.write(b);
+// }
+// if (c > a & c > b) {
+//     document.write(c);
+// } 
